a2b3a3b42e338b193c1a73be7a94a0c5d377ef29
Frederic Jacob <frederic.jacob.78@gmail.com>
2016-04-29 17:55:21 -0400

    Finish restore/backup scripts

--- backup.py
+++ backup.py
@@ -1,33 +1,49 @@
 #!/usr/bin/python
+import argparse
+import datetime
 import json
 import sys
 import requests
 
 def backup(server, filename, root_psw):
-    url = 'http://' + server + '/nhlplayoffs/api/v2.0/backup'
-    headers = {'content-type': 'application/json'}
-    data = {'root_psw':root_psw}
-    r = requests.post(url, data = json.dumps(data), headers=headers)
-    if not r.ok:
-        print('Invalid request!!!!')
+    try:
+        url = 'http://' + server + '/nhlplayoffs/api/v2.0/backup'
+        headers = {'content-type': 'application/json'}
+        data = {'root_psw':root_psw}
+        r = requests.post(url, data = json.dumps(data), headers=headers)
+        if not r.ok:
+            print('Invalid request!!!!')
+            return False
+        with open(filename, 'w') as outfile:
+            json.dump(r.json(), outfile, indent=4, sort_keys=True)
+        print('Backup successful to ' + filename)
+        return True
+    except Exception as e:
+        print(e)
         return False
-    with open(filename, 'w') as outfile:
-        json.dump(r.json(), outfile, indent=4, sort_keys=True)
-    print('Backup successful')
-    return True
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('backup.py <filename> <root_psw>')
-        exit(1)
-    filename = sys.argv[1]
-    root_psw = sys.argv[2]
+    parser = argparse.ArgumentParser(description='Backup the nhlpool database')
+    parser.add_argument('root_psw', metavar='password',
+                       help='The root password')
+    parser.add_argument('-f','--filename', metavar='filename', default='<auto>', nargs='?',
+                       help='The name of the file to backup to')
+    parser.add_argument('-s', '--server', metavar='server', default='debug', nargs='?',
+                       help='The server to use')
 
-    if len(sys.argv) > 3 and sys.argv[3] == 'prod':
+    args = parser.parse_args()
+
+    if args.server == 'prod':
         print('Using production server')
         server = 'nhlpool.herokuapp.com/'
     else:
         print('Using debug server')
         server = 'localhost:5000'
 
-    backup(server, filename, root_psw)
+    filename = args.filename
+    if args.filename == '<auto>':
+        print('Using auto filename')
+        n=datetime.datetime.now()
+        args.filename = 'backup-' +server + '-' + n.isoformat() + '.backup'
+
+    backup(server, args.filename, args.root_psw)
--- restore.py
+++ restore.py
@@ -1,39 +1,50 @@
 #!/usr/bin/python
+import argparse
 import json
 import sys
 import requests
 
 def restore(server, filename, root_psw):
-    with open(filename, 'r') as infile:
-        data = json.load(infile)
+    try:
+        with open(filename, 'r') as infile:
+            data = json.load(infile)
 
-    url = 'http://' + server + '/nhlplayoffs/api/v2.0/restore'
-    headers = {'content-type': 'application/json'}
-    data = {'root_psw':root_psw, 'data':data}
-    r = requests.post(url, data = json.dumps(data), headers=headers)
-    if not r.ok:
-        print('Invalid request!!!!')
-        return False
+        url = 'http://' + server + '/nhlplayoffs/api/v2.0/restore'
+        headers = {'content-type': 'application/json'}
+        data = {'root_psw':root_psw, 'data':data}
+        r = requests.post(url, data = json.dumps(data), headers=headers)
+        if not r.ok:
+            print('Invalid request!!!!')
+            return False
 
-    if r.json()['result']:
-        print('Restore successful')
-        return True
-    else:
-        print('Invalid parameter');
+        if r.json()['result']:
+            print('Restore successful')
+            return True
+        else:
+            print('Invalid parameter');
+            return False
+    except Exception as e:
+        print(e)
         return False
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('restore.py <filename> <root_psw>')
-        exit(1)
-    filename = sys.argv[1]
-    root_psw = sys.argv[2]
+    parser = argparse.ArgumentParser(description='Restore an nhlpool database backup')
+    parser.add_argument('root_psw', metavar='password',
+                       help='The root password')
+    parser.add_argument('filename', metavar='filename',
+                       help='The name of the file to restore')
+    parser.add_argument('-s', '--server', metavar='server', default='debug', nargs='?',
+                       help='The server to use')
 
-    if len(sys.argv) > 3 and sys.argv[3] == 'prod':
+    args = parser.parse_args()
+
+    if args.server == 'prod':
         print('Using production server')
         server = 'nhlpool.herokuapp.com/'
     else:
         print('Using debug server')
         server = 'localhost:5000'
 
-    restore(server, filename, root_psw)
+    var = raw_input("Are you sure you want to errase all data on {server} and replace it with backup {backup}? ".format(server=server,backup=args.filename))
+    if var == 'y':
+        restore(server, args.filename, args.root_psw)
